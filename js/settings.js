6b34a592dc5bd6a087a9cc4b62061bfd957da39d
RohanShedage01 <shedagerohan21@gmail.com>
2025-12-11 18:29:20 +0530

    setting section added

---
+++ js/settings.js
@@ -0,0 +1,17 @@
+function saveProfile() {
+    alert("Profile updated successfully!");
+}
+
+function changePassword() {
+    alert("Password updated successfully!");
+}
+
+// DARK MODE
+function toggleTheme() {
+    document.body.classList.toggle("dark-mode");
+}
+
+// NOTIFICATION SETTINGS
+function saveNotifications() {
+    alert("Notification settings saved!");
+}
